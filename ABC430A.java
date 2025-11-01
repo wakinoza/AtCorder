ef569cab7ba5ace4b56986435cf599e0c06ad88b
azu-wakinoza <wakinoza@gmail.com>
2025-11-01 21:06:07 +0900

    ABC430Aの修正

--- ABC430A.java
+++ ABC430A.java
@@ -3,8 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a 
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
         scanner.close();
 
+        if (c >= a) {
+          if (d < b) {
+            System.out.println("Yes");
+            return;
+          }
+        }
+        System.out.println("No");
     }
-} 
+}  
