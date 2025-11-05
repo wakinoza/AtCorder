b4aad2a667870e883e9a49cac30427bffde53d7b
azu-wakinoza <wakinoza@gmail.com>
2025-11-05 15:42:37 +0900

    クラス名の変更

--- ABC420B.java
+++ ABC420B.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class ABC420B {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
