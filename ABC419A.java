e64f0f2cce49805df35f85ccd8e183800f868e49
azu-wakinoza <wakinoza@gmail.com>
2025-11-06 16:31:17 +0900

    ABC419A

---
+++ ABC419A.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class Main {
+      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        scanner.close();
+        switch (s) {
+            case "red" ->{
+                System.out.println("SSS");
+            }
+            case "blue" ->{
+                System.out.println("FFF");
+            }
+            case "green" ->{
+                System.out.println("MMM");
+            }
+            default ->{
+                System.out.println(" Unknown");
+            }
+        }
+    }
+} 
+
+
