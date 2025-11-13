888f0320be639f0ed4ee6e554aff3cdb7265f0d2
azu-wakinoza <wakinoza@gmail.com>
2025-11-13 15:55:04 +0900

    Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 
+import java.util.*;
+
+//ABC
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s = scanner.next();
         scanner.close();
 
     }
-} 
-
-import java.util.*;
+    
